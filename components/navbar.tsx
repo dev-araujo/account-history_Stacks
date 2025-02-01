955cc32044ee7661b77b660b577e3ba1419c7ecc
dev-araujo <theriancirculo@protonmail.com>
2025-02-01 20:14:37 -0300

    feat: add placeholder input

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -75,7 +75,7 @@ export function Navbar() {
         <div className="flex-grow md:max-w-md">
           <input
             type="text"
-            placeholder="SP..."
+            placeholder="your address wallet here SP..."
             className="w-full rounded-lg bg-gray-700 px-4 py-2 text-sm"
             value={searchAddress}
             onChange={(e) => setSearchAddress(e.target.value)}
